25458d9597a2d589bfc358ec7729b9ac51811b7b
rahma2005 <rhmarhma360@gmail.com>
2026-06-19 13:48:47 +0300

    .

--- Elevate.FitnessApp/Program.cs
+++ Elevate.FitnessApp/Program.cs
@@ -19,7 +19,7 @@ namespace Elevate.FitnessApp
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+            var app = builder.Build(); 
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
