458d65872bce5691ff368c0f354a797411046016
rahma2005 <rhmarhma360@gmail.com>
2026-06-24 22:35:44 +0300

    add exceptionhandlerMiddleware, apiResponse configuration, alter user table (add identity) and add regisster endpoint

---
+++ AuthService/BuildingBlocks/Extention/MiddlewareExtensions.cs
@@ -0,0 +1,14 @@
+﻿using AuthService.BuildingBlocks.Middleware;
+
+namespace AuthService.BuildingBlocks.Extention
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder
+            UseGlobalExceptionMiddleware(
+            this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<ExceptionHandlingMiddleware>();
+        }
+    }
+}
