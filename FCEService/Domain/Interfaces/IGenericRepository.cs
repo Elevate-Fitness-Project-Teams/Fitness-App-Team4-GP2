466afd745cd02a275df93d2ae1d1a9f08e8d3c23
Sara Maged <Sara Maged@DESKTOP-JJMISKB>
2026-06-30 07:02:01 +0300

    feat(fce): Update IGenericRepo and add IFitnessPlanConfigRepositort

--- FCEService/Domain/Interfaces/IGenericRepository.cs
+++ FCEService/Domain/Interfaces/IGenericRepository.cs
@@ -6,9 +6,25 @@ namespace FCEService.Domain.Interfaces
     {
         Task<T?> GetByIdAsync(object id, CancellationToken ct = default);
         Task<IReadOnlyList<T>> GetAllAsync(CancellationToken ct = default);
-        Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default);
+
+        Task<T?> FirstOrDefaultAsync(
+            Expression<Func<T, bool>> predicate,
+            CancellationToken ct = default);
+
+        Task<T?> FirstOrderedAsync(
+            Expression<Func<T, bool>> predicate,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy,
+            CancellationToken ct = default);
+
+        Task<IReadOnlyList<T>> WhereAsync(
+            Expression<Func<T, bool>> predicate,
+            CancellationToken ct = default);
+
         Task AddAsync(T entity, CancellationToken ct = default);
-        void Update(T entity);
-        void Delete(T entity);
+
+   
+        void SaveInclude(T entity, params string[] updatedProperties);
+
+     
     }
 }
