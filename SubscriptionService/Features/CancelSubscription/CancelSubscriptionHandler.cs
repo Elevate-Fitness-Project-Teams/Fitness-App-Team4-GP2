4c192e323737d5ace57ffbd1a2bdd7b272999ae7
Shahd <shahd.mostafa12@gmail.com>
2026-07-07 03:52:55 +0300

    Add cancel subscription feature
    Introduced a `CancelSubscription` endpoint, along with `CancelSubscriptionCommand` and `CancelSubscriptionHandler` to handle subscription cancellations. The handler validates the subscription status and updates the `AutoRenew` property to `false`.
    
    Added an `AutoRenew` property to the `UserSubscription` entity and created a migration (`20260707004309_AutoRenewColumn`) to add the corresponding column to the database. Updated the `SubscriptionDbContextModelSnapshot` to reflect this change.

---
+++ SubscriptionService/Features/CancelSubscription/CancelSubscriptionHandler.cs
@@ -0,0 +1,31 @@
+﻿using BuildingBlocks.Shared.Results;
+using MediatR;
+using SubscriptionService.Domain.Contracts.Repositories;
+using SubscriptionService.Domain.Entities;
+
+namespace SubscriptionService.Features.CancelSubscription
+{
+    public class CancelSubscriptionHandler : IRequestHandler<CancelSubscriptionCommand, Result<bool>>
+    {
+        private readonly IGenericRepository<UserSubscription> _subscriptionRepository;
+
+        public CancelSubscriptionHandler(IGenericRepository<UserSubscription> subscriptionRepository)
+        {
+            _subscriptionRepository = subscriptionRepository;
+        }
+        public async Task<Result<bool>> Handle(CancelSubscriptionCommand request, CancellationToken cancellationToken)
+        {
+            var subscription = await _subscriptionRepository.GetOneAsync(x => x.UserId == request.UserId && x.Status == "Active");
+            if (subscription is null)
+                return Error.NotFound("NotFound", "RES_NOT_FOUND");
+
+            if (!subscription.AutoRenew)
+                return Error.Conflict("Conflict", "RES_ALREADY_CANCELED");
+
+            subscription.AutoRenew = false;
+            await _subscriptionRepository.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
