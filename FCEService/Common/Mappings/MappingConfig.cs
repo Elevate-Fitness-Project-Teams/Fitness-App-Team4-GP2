a13a62044991dbd5c352ccd928594ebb66513f28
Sara Maged <Sara Maged@DESKTOP-JJMISKB>
2026-07-09 22:01:17 +0300

    Update MappingConfig

--- FCEService/Common/Mappings/MappingConfig.cs
+++ FCEService/Common/Mappings/MappingConfig.cs
@@ -1,5 +1,7 @@
 ﻿using FCEService.Domain.Entities;
 using FCEService.Features.CalculateMetrics;
+using FCEService.Features.GetFitnessMetrics;
+using FCEService.Features.GetFitnessStats;
 using FCEService.Features.SaveFitnessStats;
 using FCEService.Features.Shared.Dtos;
 using Mapster;
@@ -16,9 +18,17 @@ namespace FCEService.Common.Mappings
                   .Map(dest => dest.Goal, src => src.Goal.ToString())
                   .Map(dest => dest.ActivityLevel, src => src.ActivityLevel.ToString());
 
+            config.NewConfig<UserFitnessStatDto, GetFitnessStatsResponse>()
+                 .Map(dest => dest.Gender, src => src.Gender.ToString())
+                 .Map(dest => dest.Goal, src => src.Goal.ToString())
+                 .Map(dest => dest.ActivityLevel, src => src.ActivityLevel.ToString());
+
 
             config.NewConfig<CalculatedMetricDto, CalculateMetricsResponse>()
                   .Map(dest => dest.Status, src => src.Status.ToString());
+
+            config.NewConfig<CalculatedMetricDto, GetFitnessMetricsResponse>()
+                 .Map(dest => dest.Status, src => src.Status.ToString());
 
         }
     }
