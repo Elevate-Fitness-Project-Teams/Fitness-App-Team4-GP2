c9ae7e8c3dbb57eb32d83c9282c0b5141c3a81ac
Sara Maged <Sara Maged@DESKTOP-JJMISKB>
2026-07-10 08:32:30 +0300

    Add DeactivateUserAssignedPlanCommand

---
+++ FCEService/Features/UserAssignedPlans/Commands/DeactivateUserAssignedPlanCommand.cs
@@ -0,0 +1,33 @@
+﻿    using BuildingBlocks.Shared.Results;
+    using FCEService.Domain.Entities;
+    using FCEService.Domain.Interfaces;
+    using MediatR;
+
+namespace FCEService.Features.UserAssignedPlans.Commands
+{
+    public sealed record DeactivateUserAssignedPlanCommand(
+            int Id, Guid UserId, string PlanId, DateTime AssignedAt) : IRequest<Result>;
+
+        public sealed class DeactivateUserAssignedPlanHandler(IFceUnitOfWork uow)
+            : IRequestHandler<DeactivateUserAssignedPlanCommand, Result>
+        {
+            public async Task<Result> Handle(DeactivateUserAssignedPlanCommand command, CancellationToken ct)
+            {
+                var repository = uow.GetRepository<UserAssignedPlan>(); 
+
+                var stub = new UserAssignedPlan
+                {
+                    Id = command.Id,
+                    UserId = command.UserId,
+                    PlanId = command.PlanId,
+                    AssignedAt = command.AssignedAt,
+                    IsActive = false
+                };
+
+                repository.SaveInclude(stub, nameof(UserAssignedPlan.IsActive));
+                await uow.SaveChangesAsync(ct); 
+                return Result.OK();
+            }
+        }
+    
+}
