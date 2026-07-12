09770598809060eae2c32571f0e239ac6c0c4906
Sara Maged <Sara Maged@DESKTOP-JJMISKB>
2026-07-12 04:59:13 +0300

    add ApiResponseServiceCollectionExtensions

---
+++ FCEService/Domain/Extensions/ApiResponseServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using BuildingBlocks.Shared.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FCEService.Domain.Extensions
+{
+   
+        public static class ApiResponseServiceCollectionExtensions
+        {
+          
+            public static IServiceCollection AddApiResponseConventions(this IServiceCollection services)
+            {
+                services.Configure<ApiBehaviorOptions>(options =>
+                {
+                    options.InvalidModelStateResponseFactory = context =>
+                    {
+                        var errors = context.ModelState
+                            .Values
+                            .SelectMany(v => v.Errors)
+                            .Select(e => e.ErrorMessage)
+                            .ToList();
+
+                        var response = new ApiResponse<object>
+                        {
+                            IsSuccess = false,
+                            Message = "Validation failed.",
+                            Data = null,
+                            StatusCode = StatusCodes.Status400BadRequest,
+                            Timestamp = DateTime.UtcNow,
+                            Errors = new Dictionary<string, List<string>> { { "ValidationErrors", errors } }
+                        };
+
+                        return new BadRequestObjectResult(response);
+                    };
+                });
+
+                return services;
+            }
+        }
+}
+
