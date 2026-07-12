a4beb0d8a733c85dd23752f323ecc53623595c32
Sara Maged <Sara Maged@DESKTOP-JJMISKB>
2026-07-12 04:46:32 +0300

    Add PersistenceServiceCollectionExtensions

---
+++ FCEService/Domain/Extensions/PersistenceServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FCEService.Domain.Interfaces;
+using FCEService.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace FCEService.Extensions
+{
+    public static class PersistenceServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<FCEDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IFceUnitOfWork,FceUnitofWork>();
+
+            return services;
+        }
+    }
+}
